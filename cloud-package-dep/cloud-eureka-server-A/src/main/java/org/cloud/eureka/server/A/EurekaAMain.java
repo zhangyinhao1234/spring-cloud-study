501b8934c6b0626b057e9cd1833031a250e7bd6a
zhangyinhao <zhangyinhao1234@163.com>
2018-02-25 08:25:07 +0800

    添加eureka集群的例子

---
+++ cloud-package-dep/cloud-eureka-server-A/src/main/java/org/cloud/eureka/server/A/EurekaAMain.java
@@ -0,0 +1,15 @@
+package org.cloud.eureka.server.A;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer // 启动一个服务注册中心提供给其他应用进行对话
+@SpringBootApplication
+public class EurekaAMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaAMain.class, args);
+	}
+}
--- cloud-package-dep/cloud-eureka-server/src/main/java/org/cloud/eureka/server/EurekaMain.java
+++ cloud-package-dep/cloud-eureka-server/src/main/java/org/cloud/eureka/server/EurekaMain.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer // 启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
-public class EurekaBMain {
+public class EurekaMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaBMain.class, args);
+		SpringApplication.run(EurekaMain.class, args);
 	}
 }
